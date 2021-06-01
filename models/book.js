0900cb7b6d9ef4e9331d60ea4544791111395286
Steven Abaco <stevenabaco@gmail.com>
2021-05-31 20:29:38 -0500

    Set allowNull and custom error for Title and Author

--- models/book.js
+++ models/book.js
@@ -1,26 +1,53 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Book extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  Book.init({
-    title: DataTypes.STRING,
-    author: DataTypes.STRING,
-    genre: DataTypes.STRING,
-    year: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Book',
-  });
-  return Book;
+	class Book extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+		}
+	}
+	Book.init(
+		{
+			title: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				validate: {
+					notNull: {
+						// custom error message
+						msg: 'Please provide a value for "title"',
+					},
+					notEmpty: {
+						// custom error message
+						msg: 'Please provide a value for "Title"',
+					},
+				},
+			},
+			author: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				validate: {
+					notNull: {
+						// custom error message
+						msg: 'Please provide a value for "title"',
+					},
+					notEmpty: {
+						// custom error message
+						msg: 'Please provide a value for "Title"',
+					},
+				},
+			},
+			genre: DataTypes.STRING,
+			year: DataTypes.INTEGER,
+		},
+		{
+			sequelize,
+			modelName: 'Book',
+		}
+	);
+	return Book;
 };
